2ec4bdfcaff4cc254b801588b625f181b65020f3
brilik <megabrilik@gmail.com>
2020-03-23 17:31:53 +0200

    already menu

--- includes/3_views/menu/header.php
+++ includes/3_views/menu/header.php
@@ -1,44 +1,31 @@
 <?php
-/**
- * $menu = $themeAR->get_items_tree_menu( 'main' );
- * <nav class="main-nav">
- * <ul class="main-nav-list">
- * foreach ( $menu[0]->childs as $item ) {
- *** $liClass = "main-nav-list__item";
- *** $aClass  = "main-nav-list__link";
- *** $title   = ( $item['title'] !== "#" ) ? $item['title'] : '';
- *** echo "<li class=" . $liClass . "><a href=" . $item['url'] . " class=$aClass>$title</a></li>";
- * }
- * </ul>
- * </nav>
- */
-?>
-<ul>
-	<li>
-		<a href="#">
-			О клубах
-			<i class="icon-arrow-down-small"></i>
-		</a>
-		<ul>
-			<li><a href="#">Правила центров</a></li>
-			<li><a href="#">Вопросы/ответы</a></li>
-			<li><a href="#">Проведение мероприятий</a></li>
-			<li><a href="#">Для инвесторов</a></li>
-		</ul>
-	</li>
-	<li><a href="#">Новости (акции)</a></li>
-	<li>
-		<a href="#">
-			Информация
-			<i class="icon-arrow-down-small"></i>
-		</a>
-		<ul>
-			<li><a href="#">Правила центров</a></li>
-			<li><a href="#">Вопросы/ответы</a></li>
-			<li><a href="#">Проведение мероприятий</a></li>
-			<li><a href="#">Для инвесторов</a></li>
-		</ul>
-	</li>
-	<li><a href="#">Как добраться</a></li>
-	<li><a href="#">библиотека игр</a></li>
-</ul>
+$menu = $themeAR->get_items_tree_menu( 'header' );
+if(!empty($menu[0]->childs)) {
+	$res = '<ul>';
+	foreach ( $menu[0]->childs as $key => $item ) {
+		$liClass = "main-nav-list__item";
+		$aClass  = "main-nav-list__link";
+		$title   = ( $item['title'] !== "#" ) ? $item['title'] : '';
+		$res     .= "<li class=\"{$liClass}\">";
+		$res     .= "<a href=\"{$item['url']}\" class=\"{$aClass}\">{$title}";
+		if ( ! empty( $menu[ $item['ID'] ] ) ) {
+			$res .= '<i class="icon-arrow-down-small"></i></a>';
+			$res .= '<ul>';
+			foreach ( $menu[ $item['ID'] ]->childs as $submenu ) {
+				if ( empty( $submenu['title'] ) ) {
+					continue;
+				}
+				$res .= '<li>';
+				$res .= "<a href=\"{$submenu['url']}\">{$submenu['title']}</a>";
+				$res .= '</li>';
+			}
+			$res .= '</ul>';
+		} else {
+			$res .= '</a>';
+		}
+		$res .= '</li>';
+	}
+	$res .= '</ul>';
+
+	echo $res;
+}
