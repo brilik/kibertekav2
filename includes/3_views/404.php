b0a707483b4747f1b73061e9bf022d67f458a3d8
brilik <megabrilik@gmail.com>
2020-03-23 16:57:02 +0200

    done

--- includes/3_views/404.php
+++ includes/3_views/404.php
@@ -1,3 +1,22 @@
-<?php
-wp_redirect( home_url() );
-exit;
+<?php $np = get_field( 'no_page', 'options' ); ?>
+<?php get_header(); ?>
+    <!-- BEGIN CONTENT -->
+
+    <main class="content">
+        <div class="error-page">
+            <div class="error-page__cnt">
+                <div class="error-page__info">
+                    <div class="error-page__title">
+                        <h1><?= $np['title']; ?></h1>
+                        <span><?= $np['error']; ?></span>
+                    </div>
+                    <?= $np['desc']; ?>
+                    <a href="<?= home_url(); ?>" class="error-page__btn btn"><?php _e('вернуться на главную','kiberteka'); ?></a>
+                </div>
+            </div>
+        </div>
+    </main>
+
+    <!-- CONTENT EOF   -->
+
+<?php get_footer(); ?>
