c9dbb7220d71de6cce6779a61c75819aa645511c
brilik <megabrilik@gmail.com>
2020-03-24 09:13:45 +0200

    Added: - enqueue script and styles; - disable emoji

--- functions.php
+++ functions.php
@@ -1,92 +1,76 @@
 <?php
-
 /*
  * $keyGoogleMap = 'AIzaSyCGLMb4BX0iIbD98PzkJXT7J8TbuBK2fq8';
  * $keyGoogleMap = 'AIzaSyBLUNaZ85lCMN7fStJw2Bi3';
 */
-
-// add_action('acf/init', function () {
-    // global $keyGoogleMap;
-    // acf_update_setting('google_api_key', $keyGoogleMap);
-// });
-
 function includes_full_files( $dir ) {
-    foreach ( glob( $dir . '/*.php' ) as $file ) {
-        include_once $file;
-    }
+	foreach ( glob( $dir . '/*.php' ) as $file ) {
+		include_once $file;
+	}
 }
 
 $include_path = get_template_directory() . '/includes';
 define( "VIEWS_DIR", $include_path . "/3_views/" );
-
 add_action( 'init', 'ar_theme_init', 10 );
-$themeAR                = $dbgAR = false;
-
+$themeAR = false;
 function ar_theme_init() {
-    global $include_path, $themeAR, $dbgAR;
-    include_once $include_path . "/theme_functions.php";
-    includes_full_files( $include_path . "/4_classes" );
-    $themeAR = new ThemeAR();
-//    include_once $include_path . "/useractions.php";
+	global $include_path, $themeAR;
+	include_once $include_path . "/theme_functions.php";
+	includes_full_files( $include_path . "/4_classes" );
+	$themeAR = new ThemeAR();
+	$themeAR->disable_emojis();
 }
 
+add_action( 'wp_head', 'ar_theme_name_style' );
+function ar_theme_name_style() {
+	$theme_uri = get_template_directory_uri() . '/assets/';
+	wp_register_style( 'theme-style1', get_stylesheet_uri() );
+	wp_register_style( 'theme-style2', $theme_uri . 'css/style2.css' );
+	wp_enqueue_style( 'theme-style1' );
+	if ( is_404() ) {
+		wp_enqueue_style( 'theme-style2' );
+	}
+}
 
 add_action( 'wp_enqueue_scripts', 'ar_theme_name_scripts', 100 );
 function ar_theme_name_scripts() {
-    $theme_uri = get_template_directory_uri() . '/assets/';
-
-    wp_register_style( 'main', $theme_uri . 'css/main.css', array(), null );
-
-    wp_deregister_script( 'jquery' );
-    wp_register_script( 'jquery', $theme_uri . 'js/libs/jquery-3.3.1.min.js', array(), null, true );
-    wp_register_script( 'jquery-migrate', $theme_uri . 'js/libs/jquery-migrate-1.2.1.min.js', array(), null, true );
-    wp_register_script( 'slick', $theme_uri . 'js/libs/slick.js', array(), null, true );
-    wp_register_script( 'jq-fancybox', $theme_uri . 'js/libs/jquery.fancybox.min.js', array(), null, true );
-    wp_register_script( 'jq-maskedinput', $theme_uri . 'js/libs/jquery.maskedinput.min.js', array(), null, true );
-    wp_register_script( 'jq-validate', $theme_uri . 'js/libs/jquery.validate.min.js', array(), null, true );
-    wp_register_script( 'additional-methods', $theme_uri . 'js/libs/additional-methods.js', array(), null, true );
-    wp_register_script( 'jq-matchHeight', $theme_uri . 'js/libs/jquery.matchHeight.js', array(), null, true );
-    wp_register_script( 'custom', $theme_uri . 'js/custom.js', array(), null, true );
-
-//    wp_enqueue_style( 'main' );
-//    wp_enqueue_script( 'jquery' );
-//    wp_enqueue_script( 'jquery-migrate' );
-//    wp_enqueue_script( 'slick' );
-//    wp_enqueue_script( 'jq-fancybox' );
-//    wp_enqueue_script( 'jq-maskedinput' );
-//    wp_enqueue_script( 'jq-validate' );
-//    wp_enqueue_script( 'additional-methods' );
-//    wp_enqueue_script( 'jq-matchHeight' );
-//    wp_enqueue_script( 'custom' );
-
+	$theme_uri = get_template_directory_uri() . '/assets/';
+	wp_register_style( 'main', $theme_uri . 'css/main.css', [], null );
+	wp_deregister_script( 'jquery' );
+	wp_register_script( 'jq', $theme_uri . 'js/jquery-3.0.0.min.js', [], null, true );
+	wp_register_script( 'jq-migrate', $theme_uri . 'js/jquery-migrate-1.4.1.min.js', [], null, true );
+	wp_register_script( 'swiper', $theme_uri . 'js/components/swiper.min.js', [], null, true );
+	wp_register_script( 'lazyload', $theme_uri . 'js/components/lazyload.min.js', [], null, true );
+	wp_register_script( 'jq-maskedinput', $theme_uri . 'js/components/jquery.maskedinput.js', [], null, true );
+	wp_register_script( 'jq-fancybox', $theme_uri . 'js/components/jquery.fancybox.js', [], null, true );
+	wp_register_script( 'jq-validate', $theme_uri . 'js/components/jquery.validate.js', [], null, true );
+	wp_register_script( 'list', $theme_uri . 'js/components/list.min.js', [], null, true );
+	wp_register_script( 'custom', $theme_uri . 'js/custom.js', [], null, true );
+	wp_register_script( 'custom', $theme_uri . 'js/custom2.js', [], null, true );
+	wp_enqueue_script( 'jq' );
+	wp_enqueue_script( 'jq-migrate' );
+	if ( is_home() || is_front_page() ) {
+		wp_enqueue_script( 'swiper' );
+		wp_enqueue_script( 'lazyload' );
+		wp_enqueue_script( 'jq-maskedinput' );
+		wp_enqueue_script( 'jq-fancybox' );
+		wp_enqueue_script( 'jq-validate' );
+	} elseif ( is_404() ) {
+		wp_enqueue_script( 'swiper' );
+		wp_enqueue_script( 'lazyload' );
+		wp_enqueue_script( 'jq-maskedinput' );
+		wp_enqueue_script( 'jq-fancybox' );
+		wp_enqueue_script( 'custom2' );
+	}
+	wp_enqueue_script( 'custom' );
 }
-
-add_filter( 'script_loader_tag', function ( $tag, $handle ) {
-
-    $handles = array(
-        'map',
-    );
-
-    foreach ( $handles as $defer_script ) {
-        if ( $defer_script === $handle ) {
-            return str_replace( ' src', ' defer="defer" src', $tag );
-        }
-    }
-
-    return $tag;
-}, 10, 2 );
-
 
 /** Подлючаем svg файлы */
-add_filter( 'upload_mimes', 'cc_mime_types2' );
-function cc_mime_types( $mimes ) {
-    $mimes['svg'] = 'image/svg+xml';
+add_filter( 'upload_mimes', 'cc_mime_types' );
+function cc_mime_types( $file_types ) {
+	$new_filetypes        = [];
+	$new_filetypes['svg'] = 'image/svg+xml';
+	$file_types           = array_merge( $file_types, $new_filetypes );
 
-    return $mimes;
-}
-function cc_mime_types2( $file_types ) {
-    $new_filetypes = array();
-    $new_filetypes['svg'] = 'image/svg+xml';
-    $file_types = array_merge($file_types, $new_filetypes );
-    return $file_types;
+	return $file_types;
 }
