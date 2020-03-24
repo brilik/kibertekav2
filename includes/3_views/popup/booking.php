3bb27c0df38bc35805a99f0440c374b942155a69
brilik <megabrilik@gmail.com>
2020-03-24 09:09:00 +0200

    added popup

---
+++ includes/3_views/popup/booking.php
@@ -0,0 +1,69 @@
+<div class="popup-overlay js-popup-box" data-popup-box="js-popup-booking">
+	<div class="popup-scroll">
+		<div class="popup-wrapper">
+			<div class="popup-holder">
+				<div class="popup js-popup">
+					<div class="window-open" id="booking-modal">
+						<div class="window-open-cont">
+							<a href="#" class="popup-close js-popup-close-btn">
+								<i class="icon-close"></i>
+							</a>
+							<div class="booking-content-wrap">
+								<div class="booking-content">
+									<form class="contact-form js--contact-form2">
+										<h4>онлайн-бронирование</h4>
+										<div class="box-field focused-field">
+											<div class="box-field__icon">
+												<i class="icon-joystick"></i>
+											</div>
+											<label class="box-field__label" for="input-complection">Комплектация</label>
+											<select class="form-control" id="input-complection" name="complection">
+												<option value="1">1</option>
+												<option value="2">2</option>
+												<option value="3">3</option>
+											</select>
+										</div>
+										<div class="box-field">
+											<div class="box-field__icon">
+												<i class="icon-group"></i>
+											</div>
+											<label class="box-field__label" for="input-num">Колличество</label>
+											<input class="form-control" type="text" id="input-num" name="num" required>
+										</div>
+										<div class="box-field">
+											<div class="box-field__icon">
+												<i class="icon-clock"></i>
+											</div>
+											<label class="box-field__label" for="input-time">Время с</label>
+											<input class="form-control" type="text" id="input-time" name="time" required>
+										</div>
+										<div class="box-field">
+											<div class="box-field__icon">
+												<i class="icon-phone"></i>
+											</div>
+											<label class="box-field__label" for="input-email">Номер телефона, либо почта для подтверждения</label>
+											<input class="form-control" type="text" id="input-email" name="email" required>
+										</div>
+										<div class="box-field">
+											<div class="box-field__icon">
+												<i class="icon-person"></i>
+											</div>
+											<label class="box-field__label" for="input-name2">Имя</label>
+											<input class="form-control" type="text" id="input-name2" name="name2" required>
+										</div>
+										<button type="submit" class="btn">Забронировать</button>
+									</form>
+									<div class="contact-form-success">
+										<i class="icon-cool"></i>
+										<span>Спасибо за вашу заявку!</span>
+										<p>Ваше сообщение отправлено. В ближайшее время мы свяжемся с вами.</p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
---
+++ includes/3_views/popup/call.php
@@ -0,0 +1,42 @@
+<div class="popup-overlay js-popup-box" data-popup-box="js-popup-call">
+	<div class="popup-scroll">
+		<div class="popup-wrapper">
+			<div class="popup-holder">
+				<div class="popup js-popup">
+					<div class="window-open" id="call-modal">
+						<div class="window-open-cont">
+							<a href="#" class="popup-close js-popup-close-btn">
+								<i class="icon-close"></i>
+							</a>
+							<div class="contact-form-wrap">
+								<form class="contact-form js--contact-form">
+									<h5 class="contact-form-title">заказать звонок</h5>
+									<div class="box-field">
+										<div class="box-field__icon">
+											<i class="icon-person"></i>
+										</div>
+										<label class="box-field__label" for="input-name">Введите имя</label>
+										<input class="form-control" type="text" id="input-name" name="name" required>
+									</div>
+									<div class="box-field">
+										<div class="box-field__icon">
+											<i class="icon-phone"></i>
+										</div>
+										<label class="box-field__label" for="input-tel">Номер телефона</label>
+										<input class="form-control" type="tel" id="input-tel" name="phone" required>
+									</div>
+									<button type="submit" class="btn">Заказать</button>
+								</form>
+								<div class="contact-form-success">
+									<i class="icon-cool"></i>
+									<span>Спасибо за вашу заявку!</span>
+									<p>Ваше сообщение отправлено. В ближайшее время мы свяжемся с вами.</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
