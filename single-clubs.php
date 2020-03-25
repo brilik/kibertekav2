3e205904491d1c0ebd063bf63307e74f1f0e952f
brilik <megabrilik@gmail.com>
2020-03-25 11:39:08 +0200

    Fixed: - add loop for valid theme

--- single-clubs.php
+++ single-clubs.php
@@ -1,21 +1,28 @@
 <?php
 global $themeAR;
 get_header();
-ar_the_view( 'section__club-main-block' );
-ar_the_view( 'section__club-booking' );
-ar_the_view( 'section__finding' );
-ar_the_view( 'section__club-advantages' );
-ar_the_view( 'section__club-prices' );
-ar_the_view( 'section__clubs' );
-ar_the_view( 'section__club-address' );
-?>
-    <script>
-        setTimeout(function () {
-            var elem = document.createElement('script');
-            elem.type = 'text/javascript';
-            elem.src = 'https://api-maps.yandex.ru/2.1/?lang=ru_RU&onload=getYaMap';
-            document.getElementsByTagName('body')[0].appendChild(elem);
-        }, 2000);
-    </script>
-<?php
+if ( have_posts() ) :
+	echo '<main class="content club-single-content">';
+	while ( have_posts() ) : the_post();
+		the_content();
+		ar_the_view( 'section__club-main-block' );
+		ar_the_view( 'section__club-booking' );
+		ar_the_view( 'section__finding' );
+		ar_the_view( 'section__club-advantages' );
+		ar_the_view( 'section__club-prices' );
+		ar_the_view( 'section__clubs' );
+		ar_the_view( 'section__club-address' );
+		?>
+        <script>
+            setTimeout(function () {
+                var elem = document.createElement('script');
+                elem.type = 'text/javascript';
+                elem.src = 'https://api-maps.yandex.ru/2.1/?lang=ru_RU&onload=getYaMap';
+                document.getElementsByTagName('body')[0].appendChild(elem);
+            }, 2000);
+        </script>
+	<?php
+	endwhile;
+	echo '</main>';
+endif;
 get_footer();
